6e0ab94ab486c2cb4f7c888a0cb256005661a131
Thomas <thomas.otxoa@gmail.com>
2019-09-17 11:54:42 +0200

    Devops not working

--- T-POO-700-T3/bootstrap/clean.sh
+++ T-POO-700-T3/bootstrap/clean.sh
@@ -4,7 +4,7 @@
 docker ps --filter status=dead --filter status=exited -aq | xargs -r docker rm -v
 
 # remove unused images:
-docker images --no-trunc | grep '<none>' | awk '{ print $3 }' | xargs -r docker rmi
+docker images --no-trunc | grep '<none>' | awk '{ print $3 }' | xargs -r docker rmi -f
 
 # remove unused volumes:
 find '/var/lib/docker/volumes/' -mindepth 1 -maxdepth 1 -type d | grep -vFf <(
