41d8bd4b68064f896fe8c36877ef554f5ed4dfc4
Thomas <thomas.otxoa@gmail.com>
2019-09-17 16:21:29 +0200

    Refactoring of user.vue and config.js

--- T-POO-700-T2/miniproj/src/components/User.vue
+++ T-POO-700-T2/miniproj/src/components/User.vue
@@ -10,14 +10,115 @@
 
     <div id='Users'>
       <button v-on:click='getUsers()'>Get Users</button>
-      <table v-model="t"></table>
+      <table></table>
     </div>
   </div>
 </template>
 
 <script>
   import axios from 'axios';
-  import {TM_URL} from "../config.js";
+  import  {
+            get_request_serv,
+            post_request_serv,
+            put_request_serv,
+            delete_request_serv
+          } from "../js/http_request.js";
+
+  ///===============================================================
+  //
+  //===============================================================
+  export default {
+    name: 'User',
+    components: {},
+    data() {
+      return {
+        user_username: '',
+        user_email: '',
+        user_id: 7,
+
+        // display errors
+        userCreationError: 0,
+        getUsersError: 0,
+        getUserError: 0,
+        userUpdateError: 0
+      };
+    },
+    methods: {
+      //===============================================================
+      // public
+      //===============================================================
+
+      //#############################################################
+      createUser: function (event) {
+        post_request_serv("users",
+                          {
+                            "user": {
+                              email: this.user_email,
+                              username: this.user_username
+                            }
+                          },
+                          (success, response) => {
+                            if(response.status == 200)
+                              console.log(response.data);
+                            else
+                              this.userCreationError = 1;
+                          });
+      },
+
+      //#############################################################
+      getUser: function (event) {
+        get_request_serv("users?email=" + this.user_email + '&username=' + this.user_username,
+                          (success, response) => {
+                            if(response.status == 200)
+                              console.log(response.data);
+                            else
+                              this.getUserError = 1;
+                          });
+      },
+
+      //#############################################################
+      getUser_byid: function (event) {
+        get_request_serv("users/" + this.user_id,
+                          (success, response) => {
+                            displayUsers_tab(response.data);
+                          });
+      },
+
+      //#############################################################
+      getUsers: function (event) {
+        get_request_serv("users",
+                          (success, response) => {
+                            displayUsers_tab(response.data);
+                          });
+      },
+
+      //#############################################################
+      updateUser: function (event) {
+        put_request_serv("users/" + this.user_id,
+                          {
+                            "user": {
+                              email: this.user_email,
+                              username: this.user_username
+                            }
+                          },
+                          (success, response) => {
+                            if(response.status == 200)
+                              console.log(rep.data);
+                            else
+                              this.userUpdateError = 1;
+                          });
+      },
+
+      //#############################################################
+      deleteUser: function (id) {
+        delete_request_serv("users/" + id,
+                            (success, response) => {
+                              alert(success);
+                            });
+      }
+    }
+  }
+
 
   //===============================================================
   // private
@@ -40,7 +141,6 @@
     td_update.innerHTML = "<button v-on:click='createUser()'>Update</button>";
     var td_delete = row.insertCell();
     td_delete.innerHTML = "<button v-on:click='deleteUser(" + user.id + ")'>Delete</button>";
-
 
     row.appendChild(td1);
     row.appendChild(td2);
@@ -69,110 +169,6 @@
     displayUsers(users, tab);
 
     div.appendChild(tab);
-  }
-
-  ///===============================================================
-  //
-  //===============================================================
-  export default {
-    name: 'User',
-    components: {},
-    data() {
-      return {
-        user_username: '',
-        user_email: '',
-        user_id: 7,
-
-        // display errors
-        userCreationError: 0,
-        getUsersError: 0,
-        getUserError: 0,
-        userUpdate: 0
-      };
-    },
-    methods: {
-      //===============================================================
-      // public
-      //===============================================================
-
-      //#############################################################
-      createUser: function (event) {
-        axios.post(TM_URL + "users", {
-                                                "user": {
-                                                  email: this.user_email,
-                                                  username: this.user_username
-                                                }
-                                              })
-        .then ((response) => {
-          if(response.status == 200)
-            console.log(response.data);
-          else
-            this.userCreationError = 1;
-        });
-      },
-
-      //#############################################################
-      getUser: function (event) {
-        axios.get(TM_URL + "users?email=" + this.user_email + '&username=' + this.user_username)
-        .then ((response) => {
-          if(response.status == 200)
-            console.log(response.data);
-          else
-            this.getUserError = 1;
-        });
-      },
-
-      //#############################################################
-      getUser_byid: function (event) {
-        axios.get(TM_URL + "users/" + this.user_id).then ((response) => {
-          if(response.status == 200) {
-            console.log(response.data);
-            displayUsers_tab(response.data);
-          }
-          elsecoucou
-            this.getUsersError = 1;
-        });
-      },
-
-      //#############################################################
-      getUsers: function (event) {
-        axios.get(TM_URL + "users").then ((response) => {
-          if(response.status == 200) {
-            console.log(response.data);
-            displayUsers_tab(response.data);
-          }
-          else
-            this.getUsersError = 1;
-        });
-      },
-      
-      //#############################################################
-      updateUser: function (event) {
-        axios.put(TM_URL + "users/" + this.user_id, {
-                                                "user": {
-                                                  email: this.user_email,
-                                                  username: this.user_username
-                                                }
-                                              })
-        .then ((response) => {
-          if(response.status == 200)
-            console.log(response.data);
-          else
-            this.userCreationError = 1;
-        });
-      },
-
-      //#############################################################
-      deleteUser: function (id) {
-        alert(id);
-        axios.delete(TM_URL + "users/" + id).then ((response) => {
-          if(response.status == 200)
-            console.log(response.data);
-          else
-            this.getUsersError = 1;
-        });
-      }
-    }
   }
 </script>
 
