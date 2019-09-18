857468f87750b3a16bc862776f8f6e1db4f2faab
Thomas <thomas.otxoa@gmail.com>
2019-09-18 13:58:13 +0200

    docker test

---
+++ T-POO-700-T3/devops_elixir/build.sh
@@ -0,0 +1,21 @@
+#if [ -d "build" ]; then
+  mkdir build
+  mkdir build/front
+  mkdir build/back
+
+  cp ../../T-POO-700-T1/elixirproj_api/* build/back -r
+  cp ../../T-POO-700-T2/miniproj/* build/front -r
+  cp Dockerfile_back build/back/Dockerfile
+  cp Dockerfile_front build/front/Dockerfile
+  cp entrypoint.sh build/back
+  cp docker-compose.yml build
+  cp .env build
+
+#fi
+
+cd build
+
+docker-compose up --build
+#docker-compose start
+
+#rm -r build
