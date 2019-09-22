5ffbc45626eff776e9bcc9e0b48da495825adb72
Thomas <thomas.otxoa@gmail.com>
2019-09-22 22:28:42 +0200

    Association team userteam user

--- T-POO-700-T1/elixirproj_api/lib/todo_list/association/user_team.ex
+++ T-POO-700-T1/elixirproj_api/lib/todo_list/association/user_team.ex
@@ -3,21 +3,16 @@ defmodule Todolist.Association.UserTeam do
   import Ecto.Changeset
 
   schema "usersteams" do
-    field :user, :id
-    field :team, :id
-
-    belongs_to :users, Todolist.Account.User
-    belongs_to :teams, Todolist.Account.Team
+    belongs_to :user, Todolist.Account.User
+    belongs_to :team, Todolist.Account.Team
 
     timestamps()
   end
 
-  @optional_fields ~w(team, user)
-
   @doc false
   def changeset(user_team, attrs) do
     user_team
-    |> cast(attrs, [])
-    |> validate_required([])
+    |> cast(attrs, [:user_id, :team_id])
+    |> validate_required([:user_id, :team_id])
   end
 end
