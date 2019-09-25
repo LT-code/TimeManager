a11e42378696946e679c92fd1b5c79e4c418605b
Thomas <thomas.otxoa@gmail.com>
2019-09-25 22:30:03 +0200

    Routes correction

--- T-POO-700-T2/miniproj/src/main.js
+++ T-POO-700-T2/miniproj/src/main.js
@@ -23,4 +23,3 @@ new Vue({
   store,
   render: h => h(App)
 }).$mount("#app");
->>>>>>> dev
--- T-POO-700-T2/miniproj/src/router/routes.js
+++
@@ -1,9 +0,0 @@
-import home from '../components/HomePage'
-import login from '../components/Authentication/Login'
-import register from '../components/Authentication/Registration'
-
-export default [
-	{ path:'/', component: home },
-	{ path:'/login', component: login },
-	{ path:'/register', component: register }
-]
