01badbe3497e17da28b58c8f9a8abd2bc6428634
Thomas <thomas.otxoa@gmail.com>
2019-09-25 23:48:48 +0200

    delete unknown files

--- PROJECT/time-manager/src/routes/routes.js
+++
@@ -1,9 +0,0 @@
-import home from '../components/HomePage'
-import login from '../components/Login'
-import register from '../components/Register'
-
-export default [
-	{ path:'/', component: home },
-	{ path:'/login', component: login },
-	{ path:'/register', component: register }
-]
