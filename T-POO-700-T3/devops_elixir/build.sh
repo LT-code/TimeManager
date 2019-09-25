d3cee05e2ac2dfbe3b75b9959bf4c820ba6871a2
Thomas <thomas.otxoa@gmail.com>
2019-09-25 12:16:07 +0200

    Front, postgres and back works

--- T-POO-700-T3/devops_elixir/build.sh
+++ T-POO-700-T3/devops_elixir/build.sh
@@ -7,7 +7,8 @@
   cp ../../T-POO-700-T2/miniproj/* build/front -r
   cp Dockerfile_back build/back/Dockerfile
   cp Dockerfile_front build/front/Dockerfile
-  cp entrypoint.sh build/back
+  cp entrypoint_back.sh build/back/entrypoint.sh
+  cp entrypoint_front.sh build/front/entrypoint.sh
   cp docker-compose.yml build
   cp .env build
   cp dev.exs build/back/config/
